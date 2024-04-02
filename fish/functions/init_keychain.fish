c195a6c73c2cc1f4a58111780df80a991a6ed5fa
Ross Fischer <skyluk@gmail.com>
2024-04-02 11:11:39 -0600

    move keychain init to function

--- fish/functions/init_keychain.fish
+++ fish/functions/init_keychain.fish
@@ -1,8 +1,6 @@
-fish_add_path /opt/homebrew/bin
-
-if status is-login
-    and status is-interactive
+function init_keychain
     # To add a key, set -Ua SSH_KEYS_TO_AUTOLOAD keypath
     # To remove a key, set -U --erase SSH_KEYS_TO_AUTOLOAD[N]
+    echo $SSH_KEYS_TO_AUTOLOAD
     keychain --quiet --eval $SSH_KEYS_TO_AUTOLOAD | source
 end
